8ff86125a68f497a92ea3315ce20896d86904519
André Baldeweg <a@baldeweg.org>
2022-05-27 19:09:16 +0200

    add tests

---
+++ tests/e2e/integration/search/reservation.spec.js
@@ -0,0 +1,34 @@
+describe('Filter', () => {
+  it('show notification', () => {
+    cy.server()
+    cy.route('GET', '**/api/me', {
+      id: 1,
+      username: 'admin',
+      roles: ['ROLE_ADMIN', 'ROLE_USER'],
+      branch: {
+        id: 1,
+      },
+      isUser: true,
+      isAdmin: true,
+    })
+    cy.route('GET', '**/api/reservation/list', [
+      {
+        id: '1',
+      },
+    ])
+    cy.route('GET', '**/api/bookmark', {})
+    cy.route('GET', '**/api/inventory', {})
+    cy.route('GET', '**/api/genre', {})
+    cy.route('GET', '**/api/condition', {})
+    cy.route('GET', '**/api/format', {})
+
+    cy.setCookie('token', 'token')
+    cy.setCookie('refresh_token', 'refresh_token')
+
+    cy.visit('/search')
+
+    cy.get('.alert.alert_success')
+
+    cy.get('.badge_content').contains('1')
+  })
+})
